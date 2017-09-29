9fef8d4481c7502128b07f8c81a65b645a71595b
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2017-09-29 13:12:32 -0400

    Update LinuxScr.sh

--- LinuxScr.sh
+++ LinuxScr.sh
@@ -2,26 +2,37 @@
 
 firewall(){
 
-  # Aptitude is used to meet the goals of the operation - in the second if statement
-  # choose what ports you want to allow prior to running this section of the script 
-  apt-get install aptitude -y /dev/null
-  if aptitude show ufw | grep -q "State: not installed"; then
-          apt-get install ufw -y
-          if ufw status | grep -q "Status: inactive"; then
-          	ufw default deny
-          	ufw enable
-          	#ufw allow 22 #SSH
-          	#ufw allow 25 #SMTP
-          	#ufw allow 110 #POP3
-          	#ufw allow 139 #Samba
-          	#ufw allow 445 #Samba
-          	#ufw allow 137 #Samba
-          	#ufw allow 138 #Samba
-          	#ufw allow 3306 #Mysql
-          else
-          	echo "Could not enable UFW."
-          fi
-  fi
+	# Aptitude is used to meet the goals of the operation - in the second if statement
+  	# choose what ports you want to allow prior to running this section of the script
+  	apt-get install aptitude -y /dev/null
+  	if aptitude show ufw | grep -q "State: not installed"; then
+        	apt-get install ufw -y
+        	if ufw status | grep -q "Status: inactive"; then
+          		ufw default deny
+          		ufw enable
+          		#ufw allow 22 #SSH
+          		#ufw allow 25 #SMTP
+          		#ufw allow 110 #POP3
+          		#ufw allow 139 #Samba
+          		#ufw allow 445 #Samba
+          		#ufw allow 137 #Samba
+          		#ufw allow 138 #Samba
+          		#ufw allow 3306 #Mysql
+          	else
+          		echo "Could not enable UFW."
+          	fi
+  	fi
+	
+  	iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 23 -j DROP         #Block Telnet
+        iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 2049 -j DROP       #Block NFS
+        iptables -A INPUT -p udp -s 0/0 -d 0/0 --dport 2049 -j DROP       #Block NFS
+        iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 6000:6009 -j DROP  #Block X-Windows
+        iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 7100 -j DROP       #Block X-Windows font server
+        iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 515 -j DROP        #Block printer port
+        iptables -A INPUT -p udp -s 0/0 -d 0/0 --dport 515 -j DROP        #Block printer port
+        iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 111 -j DROP        #Block Sun rpc/NFS
+        iptables -A INPUT -p udp -s 0/0 -d 0/0 --dport 111 -j DROP        #Block Sun rpc/NFS
+        iptables -A INPUT -p all -s localhost  -i eth0 -j DROP            #Deny outside packets from internet which claim to be from your loopback interface.
 }
 
 sys(){
