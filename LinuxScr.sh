39acd68c9fc9140a82423778d9abfbcc8244dd28
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2017-10-01 10:21:14 -0400

    Update LinuxScr.sh

--- LinuxScr.sh
+++ LinuxScr.sh
@@ -104,6 +104,55 @@ media(){
  	find / -name "*.avi" -type f -delete
 }
 
+hack(){
+
+rmhack(){
+	
+	apt-get remove nmap	 # Scanning
+	apt-get remove dnmap     # Scanning
+	apt-get remove telnet    # Insecure Transfer
+	apt-get remove bind9     # DNS 
+	apt-get remove apache2   # Webserver
+	apt-get remove vsftpd    # FTP
+	apt-get remove ssh       # SecureShell
+	apt-get remove samba     # File Sharinga
+	apt-get remove mysql     # Database
+	apt-get remove mariadb   # Database
+	apt-get remove jetty     # Webserver
+	apt-get remove nginx     # Webserver
+	
+	### Removes all of the hacking tools associated with the kali-linux operating system ###
+	
+	apt-get autoremove --purge --force-yes -y airbase-ng acccheck ace-voip amap apache-users arachni android-sdk apktool arduino armitage asleap automater \
+	backdoor-factory bbqsql bed beef bing-ip2hosts binwalk blindelephant bluelog bluemaho bluepot blueranger bluesnarfer bulk-extractor \
+	bully burpsuite braa \
+	capstone casefile cdpsnarf cewl chntpw cisco-auditing-tool cisco-global-exploiter cisco-ocs cisco-torch cisco-router-config cmospwd \
+	cookie-cadger commix cowpatty crackle creddump crunch cryptcat cymothoa copy-router-config cuckoo cutycapt \
+	davtest dbd dbpwaudit dc3dd ddrescue deblaze dex2jar dff dhcpig dictstat dirb dirbuster distorm3 dmitry dns2tcp dnschef dnsenum \
+	dnsmap dnsrecon dnstracer dnswalk doona dos2unix dotdotpwn dradis dumpzilla \
+	eapmd5pass edb-debugger enum4linux enumiax exploitdb extundelete \
+	fern-wifi-cracker fierce fiked fimap findmyhash firewalk fragroute foremost funkload \
+	galleta ghost-fisher giskismet grabber go-lismero goofile gpp-decrypt gsad gsd gqrx guymager gr-scan \
+	hamster-sidejack hash-identifier hexinject hexorbase http-tunnel httptunnel hping3 hydra \
+	iaxflood inguma intrace inundator inviteflood ipv6-toolkit iphone-backup-analyzer intersect ismtp isr-evilgrade \
+	jad javasnoop jboss-autopwn jd-gui john johnny joomscan jsql \
+	kalibrate-rtl keepnote killerbee kismet keimpx \
+	linux-exploit-suggester ldb lynis \
+	maltego-teeth magictree masscan maskgen maskprocessor mdk3 metagoofil metasploit mfcuk mfoc mfterm miranda mitmproxy multiforcer \
+	multimon-ng \
+	ncrack netcat nishang nipper-ng ntop \
+	oclgausscrack ohwurm ollydpg openvas-administrator openvas-cli openvas-manager openvas-scanner oscanner \
+	p0f padbuster paros parsero patator pdf-parser pdfid pdgmail peepdf phrasendrescher pipal pixiewps plecost polenum policygen \
+	powerfuzzer powersploit protos-sip proxystrike pwnat \
+	rcrack rcrack-mt reaver rebind recon-ng redfang regripper responder ridenum rsmangler rtlsdr-scanner rtpbreak rtpflood rtpinsertsound \
+	rtpmixsound \
+	sakis3g sbd sctpscan setoolkit sfuzz shellnoob sidguesser siparmyknife sipp sipvicious skipfish slowhttptest smali smtp-user-enum \
+	sniffjoke snmpcheck spooftootph sslcaudit sslsplit sslstrip sslyze sqldict sqlmap sqlninja sqlsus statprocessor \
+	t50 termineter thc-hydra thc-ipv6 thc-pptp-bruter thc-ssl-dos tnscmd10g truecrack theharverster tlssled twofi \
+	u3-pwn uatester urlcrazy uniscan unix-privesc-check vega w3af webscarab webshag webshells webslayer websploit weevely wfuzz wifi-honey \
+	wifitap wifite wireshark winexe wpscan wordlists valgrind volatility voiphopper wol-e xspy xplico xsser yara yersinia zaproxy
+}
+
 ssh(){
 	
 	### Enables and creates a MOTD Banner message ###
