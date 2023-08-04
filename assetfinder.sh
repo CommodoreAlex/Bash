077bdde0100b5334ffd464c73c8a1d127c4bad18
ACommodore <CommodoreAlex@users.noreply.github.com>
2023-08-03 23:56:51 -0400

    Create assetfinder.sh

---
+++ assetfinder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+$url=$1
+
+# running assetfinder and putting everything into assets.txt
+
+if [ ! -d "$url" ]; then
+	mkdir $url
+fi
+
+if [ ! -d "$url/recon" ]; then
+	mkdir $url/recon
+fi
+
+# this will only grep out whats in $1
+
+echo "[+] Harvesting subdomains with assetfinder..."
+assetfinder $url >> $url/recon/assets.txt
+cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
+rm $url/recon/assets.txt
+
+# this has a final text stored in that folder
