66b235b6bfe3e60b0a4d3d36dc80da0919993c71
Alexander A. Commodore <CommodoreAlex@users.noreply.github.com>
2025-05-30 15:42:18 -0400

    Update hackerrank_grep1.sh

--- Grep/hackerrank_grep1.sh
+++ Grep/hackerrank_grep1.sh
@@ -1,1 +1,4 @@
-grep -i -E 'the|that|then|those'
+# Case insensitivty (-i)
+# Extended Regex (-E) for multiple words
+# Word boundaries (\b) to match whole words only, avoiding partial matches like other
+grep -i -E '\b(the|that|then|those)\b'
