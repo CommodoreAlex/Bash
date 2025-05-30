8b1e710f32dbc19bbc1ec7e9b63c0679d719a572
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2025-05-30 16:24:16 -0400

    Delete Grep directory

--- Grep/hackerrank_grep1.sh
+++
@@ -1,4 +0,0 @@
-# Case insensitivty (-i)
-# Extended Regex (-E) for multiple words
-# Word boundaries (\b) to match whole words only, avoiding partial matches like other
-grep -i -E '\b(the|that|then|those)\b'
